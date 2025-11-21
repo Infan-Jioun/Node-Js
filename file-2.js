6af8fa39ad59e14a788b238d3a3521b8b02ce160
Infan-Jioun <infanjiounrahman20606@gmail.com>
2025-11-21 11:39:10 +0600

    added file and index.js

--- file-2.js
+++ file-2.js
@@ -1,4 +1,5 @@
 const { a } = require("./files-1")
 const { a: x } = require("./file-3")
-
-console.log(a, x);
+const { add, subs } = require("./utils")
+console.log(add(2, 4));
+console.log(subs(2, 4));
---
+++ utils/subs.js
@@ -0,0 +1,4 @@
+function subs(x, y) {
+    return x - y;
+}
+module.exports = { subs };
